f3ecccf137f2c751e5477487979b1b065a25c099
zhangxv <xuzhang@thoughtworks.com>
2015-02-27 06:07:49 +0800

    Print one asterisk to the console

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    private static void printAsterisk(int n) {
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.print('*');
+    }
+}
