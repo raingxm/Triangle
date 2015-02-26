98cee1b125a2bc73f2036041fe4ea02e52654385
zhangxv <xuzhang@thoughtworks.com>
2015-02-27 06:10:17 +0800

    draw a horizontal line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
 public class Main {
-    private static void printAsterisk(int n) {
-
+    private static void printAsteriskInHorizonLine(int n) {
+        for(int i=0; i<n; i++) {
+            System.out.print('*');
+        }
     }
 
 
     public static void main(String[] args) {
-        System.out.print('*');
+        printAsteriskInHorizonLine(3);
     }
 }
